34e66c91161bea852ee4413950dddb0b8732df9f
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-02-28 10:56:06 +0000

    Updated the Distance Converter
    added a miles to feet converter

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,20 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public void Run()
+        {
+            //first step
+            Console.WriteLine(" Please enter the number of distance: ");
+            string input = Console.ReadLine();
+            int miles = Convert.ToInt32(input);
+            Console.WriteLine(miles);
+
+            //step 2- Process
+            const int FEET_TO_MILES = 5200;
+            int feet = miles * FEET_TO_MILES;
+
+            //step 3 - output 
+            Console.WriteLine(miles + " mile is " + feet + "feet is ");
+        }
     }
 }
