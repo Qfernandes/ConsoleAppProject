ca17feb7cf82a83bb46d1c27d716fd7a9bc9fc92
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-02-28 16:56:41 +0000

    Updated Distance Conversion
    added a conversion for
    miles to feet
    feet to miles
    miles to metres

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,20 +10,59 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        const int Miles_To_Feet = 5280;
+        const int Feet_To_Miles = 5280;
+        const double Miles_To_Metres = 1609.34;
+        private double Miles;
+        private double Feets;
+        private double Metres;
+  
         public void Run()
         {
+            MilesToFeet();
+            FeetToMiles();
+            MilesToMetres();
+        }
+
+        public void MilesToFeet()
+        {
             //first step
-            Console.WriteLine(" Please enter the number of distance: ");
-            string input = Console.ReadLine();
-            int miles = Convert.ToInt32(input);
-            Console.WriteLine(miles);
+            Console.WriteLine(" Please enter the number of distance miles: ");
+            Miles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Miles is: " + Miles);
 
             //step 2- Process
-            const int FEET_TO_MILES = 5200;
-            int feet = miles * FEET_TO_MILES;
+            Feets = Miles * Miles_To_Feet;
 
             //step 3 - output 
-            Console.WriteLine(miles + " mile is " + feet + "feet is ");
+            Console.WriteLine(Miles + " miles is " + Feets + " feets ");
+        }
+        public void FeetToMiles()
+        {
+            //step 4
+            Console.WriteLine("Enter the number of distance feets: ");
+            int Feets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Feet is entered: " + Feets);
+
+            //step 5
+            Miles = Feets / Feet_To_Miles;
+
+            //step 6
+            Console.WriteLine(Feets + "feets is " + Miles + " miles " );
+        }
+
+        public void MilesToMetres()
+        {
+            //step 7
+            Console.WriteLine("Enter the number of distance miles: ");
+            double metres = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Miles entered is: ");
+
+            //step 8
+            Metres = Miles * Miles_To_Metres;
+
+            //step 9
+            Console.WriteLine(Miles + " miles is " + Metres + " metres ");
         }
     }
 }
