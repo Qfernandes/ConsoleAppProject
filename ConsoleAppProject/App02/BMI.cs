a7bcae6aab4da13d5c1ace9a1d607d4f584f3c80
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-03-01 16:20:49 +0000

    Updated BMI
    prompts the user to enter weight and height
    caluates the bmi
    outputs the weight status

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -8,5 +9,52 @@
     /// </author>
     public class BMI
     {
+        double weightKG;
+        double heightM;
+        double bmiResult;
+
+        public void Run()
+        {
+            Console.WriteLine(" Welcome To App 2 ");
+
+            Console.WriteLine("Please enter the weight: ");
+            weightKG = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the height: ");
+            heightM = Convert.ToDouble(Console.ReadLine());
+
+            bmiResult = weightKG / (heightM * heightM);
+
+            Console.WriteLine(" BMI Result is: " + bmiResult );
+
+            if (bmiResult<=18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+
+            else if (bmiResult>= 18.5 && bmiResult <= 24.9)
+            {
+                Console.WriteLine("Healthy");
+            }
+
+            else if (bmiResult>= 25.0 && bmiResult <=29.9)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else if (bmiResult>= 30.0 && bmiResult <= 34.9)
+            {
+                Console.WriteLine("Obese Class I");
+            }
+            else if (bmiResult>= 35.0 && bmiResult <= 39.9)
+            {
+                Console.WriteLine("Obese Class II");
+            }
+            else if (bmiResult>=40.0)
+            {
+                Console.WriteLine("Obese Class III");
+            }
+
+
+        }
     }
 }
