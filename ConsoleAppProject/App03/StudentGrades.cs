e87832bf16194e311404ddf397e1f26b06a38f8f
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-03-15 09:41:47 +0000

    declared constants and added choices

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -11,12 +11,61 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
+
+        public string [] Students;
+        public int [] Marks;
+        public int [] GradeProfile;
+        public double Mean;
+        public int MinimumSM;
+        public int MaximumSM;
+
+        public const int GradeA = 70;
+        public const int GradeB = 60;
+        public const int GradeC = 50;
+        public const int GradeD = 40;
+        
+        public const int HighestMark = 100;
+        public const int LowestMark = 0;
+
+
+
         public void Run()
         {
-        Console.WriteLine("Students grade");
+            Console.WriteLine("Welcome To App 3- Student Grades and Marks");
 
+            choicemenu();
         }
-        
 
+        public void choicemenu()
+        {
+            Console.WriteLine(" Please choose the following options: ");
+            Console.WriteLine(" 1. Input Student marks ");
+            Console.WriteLine(" 2. Output Marks ");
+            Console.WriteLine(" 3. Output Statistics ");
+            Console.WriteLine(" 4. Output Grade Profile ");
+
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == 1)
+            {
+                StudentMarks();
+            }
+            if (choice == 2)
+            {
+                OutputStudent();
+            }
+            if (choice == 3)
+            {
+                OutputStats();
+            }
+            if (choice == 4)
+            {
+                OutputGradeProfile();
+            }
+            
+        }
     }
 }
+
+        
