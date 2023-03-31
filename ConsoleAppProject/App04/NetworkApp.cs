37cb4d88fa91989ac706281a24af5d56535c4632
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-03-31 05:56:52 +0100

    Added choices for the networkapp

--- ConsoleAppProject/App04/NetworkApp.cs
+++ ConsoleAppProject/App04/NetworkApp.cs
@@ -21,11 +21,16 @@ namespace ConsoleAppProject.App04
             //added choices, if 1 is pressed, Imperial unit method will run, if 2 is pressed, Metric unit method will run.
             if (choice == 1)
             {
-                postimage();
+                PostImage();
 
             }
 
             if (choice == 2)
+            {
+                PostMessage();
+            }
+            
+            if (choice == 3)
             {
                 DisplayAll();
             }
@@ -34,18 +39,36 @@ namespace ConsoleAppProject.App04
         {
            news.Display(); 
         }
-        public void postimage()
+        public void PostMessage()
+        {
+            Console.WriteLine("Post your Message here ");
+
+            string author = InputName();
+
+            Console.WriteLine("Post your Message here ");
+            string message = Console.ReadLine();
+
+            MessagePost post = new MessagePost(author, message);
+            news.AddMessagePost(post);
+
+            Console.WriteLine("You have successfully posed an message ");
+            post.Display();
+
+        }
+        public void PostImage()
         {
             Console.WriteLine("Post your Image here ");
+
             string author = InputName();
 
             Console.WriteLine("File Name of your Post ");
-            string filename = InputName();
+            string filename = Console.ReadLine();
 
             Console.WriteLine("Your Image Caption ");
-            string caption = InputName();
+            string caption = Console.ReadLine();
 
             PhotoPost post = new PhotoPost(author, filename, caption);
+            news.AddPhotoPost(post);
 
             Console.WriteLine("Your Post has been successfully posted ");
             post.Display();
