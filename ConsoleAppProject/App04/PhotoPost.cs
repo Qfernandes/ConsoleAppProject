d521405a5b7ac85919cdf7bf87467a52ba10cda9
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2023-03-31 05:38:36 +0100

    Created a PhotoPost

--- ConsoleAppProject/App04/PhotoPost.cs
+++ ConsoleAppProject/App04/PhotoPost.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-
 
 namespace ConsoleAppProject.App04
 {
@@ -13,22 +11,20 @@ namespace ConsoleAppProject.App04
     /// Michael Kölling and David J. Barnes
     /// @version 0.1
     /// </author>
-    public class PhotoPost
+
+    ///<summary>
+    /// I've inherited the Post so it can communicate and access the Post class
+    /// This class is where the filename, caption, author and time is created.
+    ///</summary>
+
+    public class PhotoPost : Post
     {
-        // username of the post's author
-        public String Username { get; set; }
-
-        // the name of the image file
-        public String Filename { get; set; }
+        // declared the filename for the image file
+        public String Filename;
         
-        // a one line image caption
-        public String Caption { get; set; }   
+        // declared the caption for displaying the caption
         
-        public DateTime Timestamp { get; }
-
-        private int likes;
-        
-        private readonly  List<String> comments;
+         public String Caption;  
 
         ///<summary>
         /// Constructor for objects of class PhotoPost.
@@ -42,113 +38,20 @@ namespace ConsoleAppProject.App04
         /// <param name="filename">
         /// The filename of the image in this post.
         /// </param>
-        public PhotoPost(String author, String filename, String caption)
+        public PhotoPost(String author, String filename, String caption):base(author)
         {
-            Username = author;
             this.Filename = filename;
             this.Caption = caption;
-            Timestamp = DateTime.Now;
-
-            likes = 0;
-            comments = new List<String>();
         }
 
-
         ///<summary>
-        /// Record one more 'Like' indication from a user.
+        /// I've used Polymorphorism and created virtual for PhotPost for displaying filename, caption.
         ///</summary>
-        public void Like()
+        public override void Display()
         {
-            likes++;
-        }
-
-
-        ///<summary>
-        /// Record that a user has withdrawn his/her 'Like' vote.
-        ///</summary>
-        public void Unlike()
-        {
-            if (likes > 0)
-            {
-                likes--;
-            }
-        }
-
-
-        ///<summary>
-        /// Add a comment to this post.
-        ///</summary>
-        /// <param name="text">
-        /// The new comment to add.
-        /// </param>
-        public void AddComment(String text)
-        {
-            comments.Add(text);
-        }
-
-
-        ///<summary>
-        /// Display the details of this post.
-        /// 
-        /// (Currently: Print to the text terminal. This is simulating display 
-        /// in a web browser for now.)
-        ///</summary>
-        public void Display()
-        {
-            Console.WriteLine();
-            Console.WriteLine($"    Author: {Username}");
             Console.WriteLine($"    Filename: [{Filename}]");
             Console.WriteLine($"    Caption: {Caption}");
-            Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
-            Console.WriteLine();
-
-            if (likes > 0)
-            {
-                Console.WriteLine($"    Likes: -  {likes}  people like this.");
-            }
-            else
-            {
-                Console.WriteLine();
-            }
-
-            if (comments.Count == 0)
-            {
-                Console.WriteLine("    No comments.");
-            }
-            else
-            {
-                Console.WriteLine($"    Comment(s): {comments.Count}  Click here to view.");
-            }
-        }
-
-
-        /// <summary>
-        /// Create a string describing a time point in the past in terms 
-        /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
-        /// Currently, only seconds and minutes are used for the string.
-        /// </summary>
-        /// <param name="time">
-        /// The time value to convert (in system milliseconds)
-        /// </param> 
-        /// <returns>
-        /// A relative time string for the given time
-        /// </returns>  
-        private String FormatElapsedTime(DateTime time)
-        {
-            DateTime current = DateTime.Now;
-            TimeSpan timePast = current - time; 
-            
-            long seconds = (long)timePast.TotalSeconds;
-            long minutes = seconds / 60;
-
-            if (minutes > 0)
-            {
-                return minutes + " minutes ago";
-            }
-            else
-            {
-                return seconds + " seconds ago";
-            }
+            base.Display();
         }
     }
 }
